5fecf790e6b65cd9709478b27ab5e0a8ae2971cc
PaolitaZamora <paolita.zam@gmail.com>
2021-09-25 18:11:34 -0500

    Crud Empleado

---
+++ Persistencia/IRepositorioEmpleado.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Dominio;
+
+namespace Persistencia
+{
+    public interface IRepositorioEmpleado
+    {
+         Empleado addEmpleado (Empleado empleado);
+         Empleado updateEmpleado (Empleado empleado);
+         
+         IEnumerable <Empleado> getAllEmpleado();
+         void deleteEmpleado (int idEmpleado);
+         Empleado GetEmpleado (int idEmpleado);
+
+    }
+}
---
+++ Persistencia/RepositorioEmpleado.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System.Linq;
+using Dominio;
+
+namespace Persistencia
+{
+    public class RepositorioEmpleado : IRepositorioEmpleado
+    {
+
+        private readonly ApplicationDbContext _appContext;
+
+        public RepositorioEmpleado(ApplicationDbContext appContext){
+            _appContext = appContext;
+        }
+
+        Empleado IRepositorioEmpleado.addEmpleado(Empleado empleado)
+        {
+            var new_empleado= _appContext.Empleados.Add(empleado);
+            _appContext.SaveChanges();
+            return new_empleado.Entity;
+        }
+
+        void IRepositorioEmpleado.deleteEmpleado(int idEmpleado)
+        {
+            var EmpleadoEncontrada= _appContext.Empleados.FirstOrDefault(
+                e => e.Id == idEmpleado
+            );
+            if (EmpleadoEncontrada == null)
+            {
+                return ;
+            }
+            _appContext.Remove(EmpleadoEncontrada);
+            _appContext.SaveChanges();
+        }
+
+        IEnumerable<Empleado> IRepositorioEmpleado.getAllEmpleado()
+        {
+            return _appContext.Empleados;
+        }
+
+        Empleado IRepositorioEmpleado.GetPersona(int idEmpleado)
+        {
+            return _appContext.Empleados.FirstOrDefault(
+                e => e.Id == idEmpleado
+            );
+        }
+
+        Empleado IRepositorioEmpleado.updateEmpleado(Empleado empleado)
+        {
+            var EmpleadoEncontrada = _appContext.Empleados.FirstOrDefault(
+                e => e.Id == empleado.Id
+            );
+            if (EmpleadoEncontrada != null)
+            {
+                EmpleadoEncontrada.Nombre= empleado.Nombre;
+                EmpleadoEncontrada.Edad= empleado.Edad;
+                EmpleadoEncontrada.Documento= empleado.Documento;
+                EmpleadoEncontrada.Telefono = empleado.Telefono;
+                EmpleadoEncontrada.CorreoElectronico = empleado.CorreoElectronico;
+                EmpleadoEncontrada.Cargo = empleado.Cargo;
+                EmpleadoEncontrada.Horario = empleado.Horario;
+                EmpleadoEncontrada.Sueldo = empleado.Sueldo;
+                _appContext.SaveChanges();
+            }
+
+            return EmpleadoEncontrada;
+        }
+    }
+}
