fa6be5d1ec7cf36bbf7441bb5f2661b3bf7fd240
Andres Argoty <andresmauricioarg@gmail.com>
2021-10-11 11:46:05 -0500

    Solucion Merge

--- Persistencia/ApplicationDbContext.cs
+++ Persistencia/ApplicationDbContext.cs
@@ -7,11 +7,9 @@ namespace Persistencia
     public class ApplicationDbContext : DbContext
     {
 
-<<<<<<< HEAD
         private const String connectionString= @"Data Source= (localdb)\MSSQLLocalDB;Initial Catalog= DatabaseX ; Integrated Security=True";
-=======
-        private const String connectionString= @"Data Source= DESKTOP-H1T053M;Initial Catalog= DatabaseX ; Integrated Security=True";
->>>>>>> 4097a16b05b1ad5a4b6b5b3b36ef60f14b4d682f
+
+
 
 
         public DbSet <Persona> Personas {get;set;}
